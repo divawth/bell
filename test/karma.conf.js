b976a4c6ad1518bc72ae499e9039184e7774aa1a
tiva <752990270@qq.com>
2018-09-29 16:03:46 +0800

    update

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,8 +1,6 @@
 module.exports = function (config) {
   config.set({
-    // 指定要运行测试的浏览器，可以指定多个。必须要安装对应的加载器(launcher)，karma 会在调起本地的浏览器。
     browsers: ['Chrome'],
-    // 指定要使用的测试框架
     frameworks: ['mocha', 'chai'],
     plugins: ['karma-mocha', 
       'karma-chai', 
@@ -10,10 +8,7 @@ module.exports = function (config) {
       'karma-coverage',
       'babel-plugin-istanbul'
     ],
-    // 这个插件会将每个测试用例的测试结果打印到命令行 console 中。
     reporters: ['coverage'],
-    // 希望执行的测试文件, 这里的文件会经过 preprocessor 处理后，通过 script 便签添加到测试页面中。
-    // 更多设置可以查看 https://karma-runner.github.io/2.0/config/files.html
     files: [
       './test.js'
     ],
@@ -27,17 +22,9 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    // 使用 webapck 对文件进行编译打包，同时配置 sourcemap 方便调试代码
     preprocessors: {
-      // './test.js': []
       './test.js': []
     },
-    // wepack 配置项
-    // webpack: webpackConfig,
-    // webpackMiddleware: {
-    //   noInfo: true
-    // },
-    // 运行一次后退出，如果设为 true，运行后会默认 watch "files" 中指定的文件，如果有修改会自动重新执行。
     singleRun: true
   })
 }
--- test/test.js
+++ test/test.js
@@ -1,1 +1,10 @@
-console.log(12)
+require('../dist/bell');
+
+describe('我的测试', function () {
+  describe('bell', function () {
+    it('run start', function () {
+      // 示例化 Vue, 此时示例对应的 DOM 元素没有在页面中。
+      console.log(21)
+    })
+  })
+})
