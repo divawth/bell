d9e5181a1825718e0a0f6876a19d0784165cd0b2
tiva <752990270@qq.com>
2018-09-29 15:49:38 +0800

    update

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,14 +4,29 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     // 指定要使用的测试框架
     frameworks: ['mocha', 'chai'],
-    plugins: ['karma-mocha', 'karma-chai', 'karma-chrome-launcher'],
+    plugins: ['karma-mocha', 
+      'karma-chai', 
+      'karma-chrome-launcher', 
+      'karma-coverage',
+      'babel-plugin-istanbul'
+    ],
     // 这个插件会将每个测试用例的测试结果打印到命令行 console 中。
-    // reporters: ['spec'],
+    reporters: ['coverage'],
     // 希望执行的测试文件, 这里的文件会经过 preprocessor 处理后，通过 script 便签添加到测试页面中。
     // 更多设置可以查看 https://karma-runner.github.io/2.0/config/files.html
     files: [
       './test.js'
     ],
+    coverageReporter: {
+      // 配置生成的代码覆盖率文件存放位置
+      dir: './coverage',
+      reporters: [
+        // 生成 lcov.info 以及 html 文件，lcov.info 该文件中包含了详细的每个文件，每行，每个函数的执行信息。
+        { type: 'lcov', subdir: '.' },
+        // 在命令行输出简要覆盖率数据
+        { type: 'text-summary' }
+      ]
+    },
     // 使用 webapck 对文件进行编译打包，同时配置 sourcemap 方便调试代码
     preprocessors: {
       // './test.js': []
--- test/test.js
+++ test/test.js
@@ -1,9 +1,1 @@
-var expect = require('chai').expect;
-
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function() {
-      expect([1,2,3].indexOf(4)).to.equal(-1); 
-    });
-  });
-});
+console.log(12)
